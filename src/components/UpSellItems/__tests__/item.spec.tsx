26d3f1ba74c6ede1c1c6e375695fdb552db658be
Lars Roettig <l.roettig@techdivision.com>
2022-02-09 00:26:11 +0100

    Add TS support

--- src/components/UpSellItems/__tests__/item.spec.tsx
+++ src/components/UpSellItems/__tests__/item.spec.tsx
@@ -1,7 +1,4 @@
-// __tests__/hidden-message.js
-// these imports are something you'd normally configure Jest to import for you
-// automatically. Learn more in the setup docs: https://testing-library.com/docs/react-testing-library/setup#cleanup
-import '@testing-library/jest-dom'
+
 // NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
 import * as React from 'react'
 import { render } from '@testing-library/react'
--- src/components/UpSellItems/item.tsx
+++ src/components/UpSellItems/item.tsx
@@ -1,37 +1,22 @@
 import * as React from 'react';
 import { GetUpSellItems_products_items_upsell_products } from "../../talons/UpSellItems/__generated__/GetUpSellItems";
-import { Link } from 'react-router-dom';
-// @ts-ignore we are required to create to typesafe util
-import Image from '@magento/venia-ui/lib/components/Image';
-// @ts-ignore we are required to create to typesafe util
-import resourceUrl from "@magento/peregrine/lib/util/makeUrl";
+
 
 type ItemsProps = {
     product: GetUpSellItems_products_items_upsell_products
 }
-const IMAGE_WIDTH = 300;
-const IMAGE_HEIGHT = 150;
 
-const IMAGE_WIDTHS = new Map()
-    .set(640, IMAGE_WIDTH)
-    .set('default', 200);
 
 const Item = ({ product }: ItemsProps) => {
-    // TODO: should fetch from backend
-    const productUrlSuffix = '.html';
-    const productLink = resourceUrl(`/${product.url_key}${productUrlSuffix || ''}`);
+
     return (
-        <Link to={productLink}>
+
             <div>
-                <Image
-                    resource={product?.small_image?.url}
-                    height={IMAGE_HEIGHT}
-                    widths={IMAGE_WIDTHS}
-                />
+
                 <p>{product.sku}</p>
 
             </div>
-        </Link>
+
     )
 }
 
